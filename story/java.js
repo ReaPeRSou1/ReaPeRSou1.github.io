a8af773cf17ded5d2d1a9e91e0ddbc48be4500c7
ReaPeRSou1 <47732269+ReaPeRSou1@users.noreply.github.com>
2020-05-22 01:30:56 -0700

    Story prototype

---
+++ story/java.js
@@ -0,0 +1,51 @@
+function open0()
+{
+    var cont=document.querySelectorAll("#contentHolder .content");
+    var back=document.querySelectorAll("#contentHolder .bg");
+    cont.forEach(function(node){
+        node.style.display="none";
+    });
+    cont[0].style.width = "100%";
+    cont[0].style.height = "120vh";
+    cont[0].style.display = "block";
+    back.forEach(function(node){
+        node.style.display="none";
+    });
+    $("#subLay0").removeClass("inactive");
+    $("#clsButton").removeClass("inactive");
+    
+}
+
+function open1()
+{
+    var cont=document.querySelectorAll("#contentHolder .content");
+    var back=document.querySelectorAll("#contentHolder .bg");
+    cont.forEach(function(node){
+        node.style.display="none";
+    });
+    cont[1].style.width = "100%";
+    cont[1].style.height = "120vh";
+    cont[1].style.display = "block";
+    back.forEach(function(node){
+        node.style.display="none";
+    });
+    $("#subLay1").removeClass("inactive");
+    $("#clsButton").removeClass("inactive");
+}
+
+function closeNav()
+{
+    var cont=document.querySelectorAll("#contentHolder .content");
+    var back=document.querySelectorAll("#contentHolder .bg");
+    cont.forEach(function(node){
+        node.style.width = "";
+        node.style.display="";
+        node.style.height = "";
+    });
+    back.forEach(function(node){
+        node.style.display="block";
+    });
+    $("#subLay0").addClass("inactive");
+    $("#subLay1").addClass("inactive");
+    $("#clsButton").addClass("inactive");
+}
