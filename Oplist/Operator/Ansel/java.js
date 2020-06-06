5486a1bf4e4815dc5fd1dc98972e8a948928d8f9
SARAAD69 <a.chernega2000@gmail.com>
2020-06-06 03:29:02 +0300

    Ansel(translated)

--- Oplist/Operator/Ansel/java.js
+++ Oplist/Operator/Ansel/java.js
@@ -4,13 +4,11 @@ function changeEliteAn(index)
 {
     var buttons=document.querySelectorAll("#info .lvlButton button");
     var content=document.querySelectorAll("#info .elite");
-    var sk_range=document.querySelectorAll('#info .skillDis')
 
     buttons.forEach(function(node){
         node.style.backgroundColor="";
         node.style.color="";
     });
-    
     buttons[index].style.color="white";
     buttons[index].style.backgroundColor="rgba(175, 175, 175, 0.3)";
 
@@ -20,11 +18,17 @@ function changeEliteAn(index)
 
     content[index].style.display="block";
 
-    sk_range.forEach(function(node){
-        node.style.display="none";
-    });
+    if(index==0){
+        document.getElementById("changing0").src="pic/rad1_e0.png"
+    } else {
+        document.getElementById("changing0").src="pic/rad1_e1.png"
+    }
 
-    sk_range[index].style.display="block";
+    if(index==0){
+        document.getElementById("changing1").src="pic/rad2_e0.png"
+    } else {
+        document.getElementById("changing1").src="pic/rad2_e1.png"
+    }
 }
 
 function changeSkillLevelAn(el, n)
@@ -34,22 +38,13 @@ function changeSkillLevelAn(el, n)
     $("#skill"+value+"num"+n).addClass("display1");
     $(".skill"+n).removeClass("active");
     $("#skill"+n+"dis"+value).addClass("active");
-    var range1=document.querySelector('#info .skillDis .range1');
-    var range2=document.querySelector('#info .skillDis .range2');
+    var range1=document.querySelector('#info .skillDis #range1');
+    var range2=document.querySelector('#info .skillDis #range2');
     if (value < 4) {
         range1.style.display="block"
         range2.style.display="none"
     } else {
         range1.style.display="none"
         range2.style.display="block"
-    }
-    var range1e1=document.querySelector('#info .skillDis .range1_e1');
-    var range2e1=document.querySelector('#info .skillDis .range2_e1');
-    if (value < 4) {
-        range1e1.style.display="block"
-        range2e1.style.display="none"
-    } else {
-        range1e1.style.display="none"
-        range2e1.style.display="block"
     }
 }
