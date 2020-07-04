47834541542c0d7ce5587afa2b267b7d0f153b6a
ReaPeRSou1 <47732269+ReaPeRSou1@users.noreply.github.com>
2020-07-03 18:58:57 -0700

    123

--- story/eventStory/java.js
+++ story/eventStory/java.js
@@ -1,107 +1,3 @@
-var slideIndex = 1;
-imgSelect(slideIndex);
-
-function plusSlides(n) 
-{
-    var int = parseInt(n);
-    slideIndex += int;
-    imgSelect(slideIndex);
-}
-
-function currentImg(n) 
-{
-    var int = parseInt(n)
-    slideIndex = int;
-    imgSelect(slideIndex);
-    var str = `${n}`;
-    $('#pageSelect option').removeAttr('selected');
-    $('#pageSelect option[value="' + str + '"]').attr('selected', 'selected'); 
-
-    if (n==$('#pageSelect option:last-child').val()) 
-    {
-        document.getElementById("next").style.visibility = 'hidden';
-    }
-    else {
-        document.getElementById("next").style.visibility = 'visible';
-    }
-
-    if (n==$('#pageSelect option:first-child').val()) 
-    {
-        document.getElementById("prev").style.visibility = 'hidden';
-    }
-    else {
-        document.getElementById("prev").style.visibility = 'visible';
-    }
-    
-
-}
-
-function imgSelect(n)
-{
-    var i;
-    var slides = document.getElementsByClassName("selector");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-
-    if (n==$('#pageSelect option:last-child').val()) 
-    {
-        document.getElementById("next").style.visibility = 'hidden';
-    }
-    else {
-        document.getElementById("next").style.visibility = 'visible';
-    }
-
-    if (n==$('#pageSelect option:first-child').val()) 
-    {
-        document.getElementById("prev").style.visibility = 'hidden';
-    }
-    else {
-        document.getElementById("prev").style.visibility = 'visible';
-    }
-}
-
-$("#next").click(function() {
-    document.getElementById("prev").style.visibility = 'visible';
-    var nextElement = $('#pageSelect > option:selected').next('option');
-    if (nextElement.length > 0) {
-      $('#pageSelect > option:selected').removeAttr('selected').next('option').prop('selected', 'selected');
-    }
-
-    var isLastElementSelected = $('#pageSelect > option:selected').index() == ($('#pageSelect > option').length - 1);
-    if (!isLastElementSelected) 
-    {     
-        document.getElementById("next").style.visibility = 'visible';
-    } 
-    else 
-    {
-        document.getElementById("next").style.visibility = 'hidden';
-    }    
-  });
-
-  $("#prev").click(function() {
-    document.getElementById("next").style.visibility = 'visible';
-    var prevElement = $('#pageSelect > option:selected').prev('option');
-    if (prevElement.length > 0) {
-      $('#pageSelect > option:selected').removeAttr('selected').prev('option').prop('selected', 'selected');
-    }
-
-    var isFirstElementSelected = $('#pageSelect > option:selected').index() == 0;
-
-    if (!isFirstElementSelected) 
-    {
-        document.getElementById("prev").style.visibility = 'visible';
-    }
-    else 
-    {
-        document.getElementById("prev").style.visibility = 'hidden';
-    }
-  });
-
-
 $(function(){
     $('#dynamic_select').on('change', function () {
         var url = $(this).val();
@@ -124,52 +20,24 @@ $(function(){
     });
 });
 
-document.addEventListener("keyup",function(e){
-    var key = e.which||e.keyCode;
-    switch(key){
-       //left arrow
-       case 37:
-        var isFirstElementSelected = $('#pageSelect > option:selected').index() == 0;
-
-        if (!isFirstElementSelected) 
+$(function(){
+    $('#dynamic_select1').on('change', function () {
+        var url = $(this).val();
+        if (url) 
         {
-          var x = document.getElementById("prev");
-          x.click();
+            window.location = url; 
         }
-       break;
-       //right arrow
-       case 39:    
-       var isLastElementSelected = $('#pageSelect > option:selected').index() == ($('#pageSelect > option').length - 1);
-
-       if (!isLastElementSelected) 
-       {     
-         var x = document.getElementById("next");
-         x.click();
-       } 
-
-       break;
-       //esc
-
- 
-    }
+        return false;
+    });
 });
 
-function clickL() {
-    var isFirstElementSelected = $('#pageSelect > option:selected').index() == 0;
-
-        if (!isFirstElementSelected) 
+$(function(){
+    $('#part1').on('change', function () {
+        var url = $(this).val();
+        if (url) 
         {
-          var x = document.getElementById("prev");
-          x.click();
+            window.location = url; 
         }
-  }
-
-  function clickR() {
-    var isLastElementSelected = $('#pageSelect > option:selected').index() == ($('#pageSelect > option').length - 1);
-
-       if (!isLastElementSelected) 
-       {     
-         var x = document.getElementById("next");
-         x.click();
-  }
-}
+        return false;
+    });
+});
